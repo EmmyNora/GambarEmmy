fb0f6620a67daa362470e5fe13fabe25c96e1adc
EmmyNora <emmynora0204@gmail.com>
2025-10-23 17:15:42 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,11 @@
+import streamlit as st
+from ultralytics import YOLO
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import time
+
 st.markdown("""
     <style>
     /* ======== BASE STYLE ======== */
